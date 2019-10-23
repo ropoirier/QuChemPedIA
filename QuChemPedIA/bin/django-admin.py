807a19b0fd276cc99f71f492cdc6cec5799df7aa
Alexandre Boudet <alexandreboudet35@gmail.com>
2019-10-23 19:49:21 +0200

    Premier commit initialisation dossier test

---
+++ QuChemPedIA/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/etudiant/Documents/M1_Informatique/Concretisation_disciplinarie/QuChemPedIA/QuChemPedIA/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
