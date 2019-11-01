95d42f819936f88ead73277acbe63f4ee9bb2f87
root <root@debian>
2019-11-01 16:41:15 +0100

    Revert "Première essai test cas15 -> fail import"
    This reverts commit a85415f1e36fced20348bac4cfa1a9dbeffb0b91.

--- QuChemPedIAProject/test/test_import_file.py
+++ QuChemPedIAProject/test/test_import_file.py
@@ -3,17 +3,14 @@ import common_qcpia.QuChemPedIA_lib.import_file_lib as ifl
 
 class ImportFileTest(unittest.TestCase):
 
-	"""Test case utilise pour tester la fonction d'import de nouvelles mollecules dans la base de donnees ElasticSearch."""
+    """
+    Test case utilisé pour tester la fonction d'import de nouvelles mollécules dans la base de données
+    ElasticSearch. 
+    """
 
-	def setUp(self):
-		"""Initialisation et declaration des fichiers JSON de test"""
-		self.cas15_path = "../../../json/"
-		self.cas15_json = "cas2.json"
-		self.cas15_id = 1
+    def setUp(self):
+        """Initialisation et déclaration des fichiers JSON de test"""
+        self.cas2_path = "../../../json/"
+        self.cas2_json = "cas2.json"
+        self.cas2_id = 1
 
-	def test_import_file(self):
-		"""Test echec"""
-		ret = import_file(self.cas15_path, self.cas15_json, self.cas15_id)
-		self.assertEqual(0,ret,"echec")
-		
-unittest.main()
