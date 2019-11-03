1dd7914e604e4d4a86191d2e9575fca000c1cb2a
Omar <omar.boubnina@etud.univ-angers.fr>
2019-11-03 17:51:55 +0100

    correction front : suppression id_log

--- QuChemPedIAProject/query_qcpia/forms/QueryForm.py
+++ QuChemPedIAProject/query_qcpia/forms/QueryForm.py
@@ -10,7 +10,7 @@ class QueryForm(forms.Form):
                ('IUPAC', 'IUPAC name'),
                ('CID', 'CID PubChem'),
                ('SMILES', 'SMILES'),
-               ('id_log', 'id_log'),
+               # ('id_log', 'id_log'),
                # ('homo_alpha_energy', 'homo_alpha_energy'),
                # ('homo_beta_energy', 'homo_beta_energy'),
                # ('lumo_alpha_energy', 'lumo_alpha_energy'),
