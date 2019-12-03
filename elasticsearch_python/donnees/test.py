6fded80e1bdf4db2c5c557609d1cd79c8989d7fb
younes <younes.rafiki@etud.univ-angers.fr>
2019-12-03 18:15:19 +0100

    Ajout ligne regex & jeu molécule de test

--- elasticsearch_python/donnees/test.py
+++ elasticsearch_python/donnees/test.py
@@ -65,8 +65,46 @@ for filename in os.listdir(directory):
         id=i, body=json.loads(docket_content))
         i = i + 1
 """
+
+# Test lecture fichier de données Json (une molécule) 
+"""
 f = open("C21H19OS4_SP.json")
 docket_content = f.read()
 # Send the data into es
 es.index(index='myindex', ignore=400, doc_type='docket',
 id=1, body=json.loads(docket_content))
+"""
+
+
+
+# Test insertion prototype molécule 
+""" 
+ins = {
+	"formula": "C21H19OS4++",
+	"inChi": "1S/C21H19OS4/c1-11-13(3)25-20(15-7-5-9-23-15)17(11)19(22)18-12(2)14(4)26-21(18)16-8-6-10-24-16/h5-10,22H,1-4H3",
+	"id_user": 2,
+    	"siblings": [{"id_log":"1t0j_moBaHZQCbKm6dnI","job":"OPT"},{"id_log":"190j_moBaHZQCbKm89lG","job":"TD"}]
+}
+
+res = es.index(index='molecules',doc_type='molecule',id=1,body=ins)
+"""
+
+
+
+s = Search(using=es, index="molecules", doc_type="molecule")
+s = s.sort('formula')
+s = s.source(['formula', 'inChi', 'siblings'])
+s = s.query('regexp',formula='c2[a-z0-9]*')
+
+s = s[0:10]
+
+
+sibling = ""
+for commit in s.execute():
+	print (commit.formula)
+	print (commit.inChi)
+	print (commit.siblings[1].id_log)
+	sibling=commit.siblings[1]
+
+
+
